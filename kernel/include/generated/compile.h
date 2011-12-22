2a6141920d8a032866be941a542a90a6aa42c184
Justin <earthbound.iap@gmail.com>
2011-12-21 19:44:57 -0800

    initial.commit

---
+++ kernel/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version Samurai-3.3.1.WARRIOR */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#Samurai-3.3.1.WARRIOR PREEMPT Tue Dec 20 05:35:51 PST 2011"
+#define LINUX_COMPILE_TIME "05:35:51"
+#define LINUX_COMPILE_BY "earthbound"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.0 (GCC) "
---
+++ kernel/include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "2.6.35.7-Samurai-3.3.1.WARRIOR"
